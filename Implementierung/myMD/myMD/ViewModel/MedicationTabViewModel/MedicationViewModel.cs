bb2ff89bc6a2e024ee2b4c8f15a9b9ddbe4510cd
Jan-Luca Vettel <janlucavettel@gmx.de>
2018-01-12 15:44:40 +0100

    no message

--- Implementierung/myMD/myMD/ViewModel/MedicationTabViewModel/MedicationViewModel.cs
+++ Implementierung/myMD/myMD/ViewModel/MedicationTabViewModel/MedicationViewModel.cs
@@ -9,17 +9,8 @@ namespace myMD.ViewModel.MedicationTabViewModel
     public class MedicationViewModel
     {
 
-        public ObservableCollection<MedicineViewModel> MedicationsList { get; }
-        public bool MedicationListIsVisible;
-
         public MedicationViewModel()
         {
-            this.MedicationsList = new ObservableCollection<MedicineViewModel>();
-            this.MedicationListIsVisible = new bool();
-
-            if (MedicationsList.Count == 0) {
-                MedicationListIsVisible = false;
-            }
         }
 
     }
