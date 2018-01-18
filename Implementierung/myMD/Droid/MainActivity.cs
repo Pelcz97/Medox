c2aeb9b52b66f9c9a150af204211e1f2b835a2ec
Philipp Pelcz <p.pelcz@icloud.com>
2018-01-18 11:51:25 +0100

    added a icon to the android version and changed the name label

--- Implementierung/myMD/Droid/MainActivity.cs
+++ Implementierung/myMD/Droid/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.Graphics;
 
 namespace myMD.Droid
 {
-    [Activity(Label = "myMD.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "myMD", Icon = "@drawable/myMDIcon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
