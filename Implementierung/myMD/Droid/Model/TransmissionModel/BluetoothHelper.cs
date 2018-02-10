7c400b45e123753f5c412acc30ee33c083901ac3
Philipp Karcher <urraj@student.kit.edu>
2018-02-10 11:54:02 +0100

    Added BluetoothHelper to access platformspecific implementation of the IBluetoothLowEnergyAdapter (Call DependencyService to get an instance of the helper (see FileHelper and Hl7ParserHelper))

---
+++ Implementierung/myMD/Droid/Model/TransmissionModel/BluetoothHelper.cs
@@ -0,0 +1,13 @@
+﻿using nexus.protocols.ble;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(myMD.Model.TransmissionModel.Droid.BluetoothHelper))]
+namespace myMD.Model.TransmissionModel.Droid
+{
+    public class BluetoothHelper : IBluetoothHelper
+    {
+        private static readonly IBluetoothLowEnergyAdapter adapter = BluetoothLowEnergyAdapter.ObtainDefaultAdapter(Android.App.Application.Context);
+
+        public IBluetoothLowEnergyAdapter Adapter => adapter;
+    }
+}
---
+++ Implementierung/myMD/iOS/Model/TransmissionModel/BluetoothHelper.cs
@@ -0,0 +1,13 @@
+﻿using nexus.protocols.ble;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(myMD.Model.TransmissionModel.iOS.BluetoothHelper))]
+namespace myMD.Model.TransmissionModel.iOS
+{
+    public class BluetoothHelper : IBluetoothHelper
+    {
+        private static readonly IBluetoothLowEnergyAdapter adapter = BluetoothLowEnergyAdapter.ObtainDefaultAdapter();
+
+        public IBluetoothLowEnergyAdapter Adapter => adapter;
+    }
+}
