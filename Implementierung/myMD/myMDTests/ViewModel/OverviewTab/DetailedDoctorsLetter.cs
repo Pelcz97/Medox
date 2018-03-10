e3442937600793acf763da1234e859964eefba2f
Philipp Pelcz <p.pelcz@icloud.com>
2018-03-10 22:25:44 +0100

    added some tests

---
+++ Implementierung/myMD/myMDTests/ViewModel/OverviewTab/DetailedDoctorsLetter.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using SQLiteNetExtensions;
+using SQLiteNetExtensions.Extensions;
+using System;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+
+
+namespace myMDTests.ViewModel.OverviewTab
+{
+    class DetailedDoctorsLetterTests
+    {
+
+    }
+}
