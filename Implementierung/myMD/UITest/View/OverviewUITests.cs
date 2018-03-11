aafa0d0aeeb3f48e874bfe0345836c38839b3e61
Philipp Pelcz <p.pelcz@icloud.com>
2018-03-11 21:06:32 +0100

    added the last UITests

--- Implementierung/myMD/UITest/View/OverviewUITests.cs
+++ Implementierung/myMD/UITest/View/OverviewUITests.cs
@@ -38,6 +38,74 @@ namespace UITest
             app.Screenshot("First screen.");
         }
 
+        /// <summary>
+        /// This Test tests the whole App for the existence of every ViewElement
+        /// It does not need any Assertion, since every Line would Crash if the searched Element is not found
+        /// </summary>
+        [Test]
+        public void EveryElementInWholeApp()
+        {
+            app.Flash("Übersicht");
+            app.Flash("Medikation");
+            app.Flash("Senden");
+            app.Flash("Profil");
+            app.SwipeRightToLeft();
+            app.Flash("Medikation");
+            app.Flash("Hinzufügen");
+            app.Tap("Hinzufügen");
+            app.Flash("Neue Medikation");
+            app.Flash("Abbrechen");
+            app.Flash("Fertig");
+            app.Flash("Information");
+            app.Flash("Name des Medikaments");
+            app.Flash("Wirkstoffmenge");
+            app.Flash("Zeitraum der Einnahme");
+            app.Flash("Beginn");
+            app.Flash("Ende");
+            app.Flash("1 mal täglich");
+            app.Flash("-");
+            app.Flash("+");
+            app.Flash("StartDatePicker");
+            app.Flash("EndDatePicker");
+            app.Tap("Abbrechen");
+            app.SwipeRightToLeft();
+            app.SwipeLeftToRight();
+            app.Flash("Medikation");
+            app.SwipeRightToLeft();
+            app.Flash("Senden");
+            app.Flash("Hinweis");
+            app.Flash("ReceiveDataButton");
+            app.Tap("ReceiveDataButton");
+            app.Flash("Geräte suchen");
+            app.Flash("0 Dateien verfügbar");
+            app.Flash("0 Arztbriefe laden");
+            app.SwipeRightToLeft();
+            app.Flash("Profil");
+            app.Flash("Name");
+            app.Flash("Vorname");
+            app.Flash("Geburtstag");
+            app.Flash("Versicherungsnummer");
+            app.Flash("Blutgruppe");
+            app.Flash("Birthday");
+            app.Flash("Bloodtype");
+            app.Flash("Bearbeiten");
+            app.Tap("Bearbeiten");
+            app.Flash("Profil bearbeiten");
+            app.Flash("Abbrechen");
+            app.Flash("Fertig");
+            app.Flash("Name");
+            app.Flash("Nachname");
+            app.Flash("Vorname");
+            app.Flash("Geburtsdatum");
+            app.Flash("Versicherungsnummer");
+            app.Flash("Blutgruppe");
+            app.Flash("BirthdayPicker");
+            app.Tap("Abbrechen");
+            app.SwipeLeftToRight();
+            app.SwipeLeftToRight();
+            app.SwipeLeftToRight();
+        }
+
     }
 }
 
